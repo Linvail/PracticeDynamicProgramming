de7c68bebc1673e9a18c32a4a58aa9ca604caac5
Yifan Jian <jurinaforever@gmail.com>
2022-06-01 02:01:07 +0800

    Add more questions

--- HouseRobber/HouseRobber.cpp
+++ HouseRobber/HouseRobber.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 using namespace std;
 
+// 198. House Robber (Medium)
 class Solution
 {
 public:
@@ -15,13 +16,9 @@ public:
         // n house
         // pick ( n - 1 ), cannot pick n
         // pick ( n - 2 ), can pick n
-        if( nums.size() == 1 )
+        if (nums.size() == 1)
         {
             return nums[0];
-        }
-        else if( nums.size() == 2 )
-        {
-            return max( nums[0], nums[1] );
         }
 
         nums[1] = max( nums[0], nums[1] );
@@ -34,12 +31,63 @@ public:
     }
 };
 
+// 213. House Robber II (Medium)
+class Solution198
+{
+public:
+    int rob(vector<int>& nums)
+    {
+        if (nums.size() == 1)
+        {
+            return nums[0];
+        }
+
+        return max(rob(nums, 0, nums.size() - 1), rob(nums, 1, nums.size()));
+    }
+
+    int rob(vector<int>& nums, int start, int end)
+    {
+        // The accumulated money from beginning to this house, robbing this house.
+        int sumRobThis = 0;
+        // The accumulated money from beginning to this house, not robbing this house.
+        int sumNotRobThis = 0;
+
+        // Q   Q   Q   Q
+        // nr  r
+        //     ^
+        for (int i = start; i < end; ++i)
+        {
+            int preSumRobThis = sumRobThis;
+            int preSumNotRobThis = sumNotRobThis;
+
+            // Going to rob this one, meaning the previous house cannot be robbed.
+            sumRobThis = preSumNotRobThis + nums[i];
+            // Not going to rob this one, so we should pick the max from the previous rob/not-rob.
+            sumNotRobThis = max(preSumRobThis, preSumNotRobThis);
+        }
+
+        return max(sumRobThis, sumNotRobThis);
+    }
+};
+
 int main()
 {
-    vector<int> testData = { 1, 2, 3, 1 };
+    // 198. House Robber (Medium)
+    // Input: nums = [1,2,3,1]
+    // Output: 4
+    // Input: nums = [2,7,9,3,1]
+    // Output: 12
+    vector<int> testData = { 1,2,3,1 };
     //vector<int> testData = { 2, 1, 1, 2 };
-
     //vector<int> testData = { 114, 117, 207, 117, 235, 82, 90, 67, 143, 146, 53, 108, 200, 91, 80, 223, 58, 170, 110, 236, 81, 90, 222, 160, 165, 195, 187, 199, 114, 235, 197, 187, 69, 129, 64, 214, 228, 78, 188, 67, 205, 94, 205, 169, 241, 202, 144, 240 };
+    std::cout << "198. House Robber: " << Solution::rob( testData ) << endl;
 
-    std::cout << "HouseRobber: " << Solution::rob( testData ) << endl;
+    // 213. House Robber II (Medium)
+    // Input: nums = [2, 3, 2]
+    // Output : 3
+    // Input: nums = [1]
+    // Output: 1
+    testData = { 1 };
+    Solution198 sol198;
+    std::cout << "213. House Robber II: " << sol198.rob(testData) << endl;
 }
--- PracticeDynamicProgramming/PracticeDynamicProgramming.cpp
+++ PracticeDynamicProgramming/PracticeDynamicProgramming.cpp
@@ -3,13 +3,253 @@
 #include "PracticeDynamicProgramming.h"
 
 #include <iostream>
+#include <vector>
+#include <unordered_set>
 
 using namespace std;
 using namespace DynamicProgramming;
+
+//---------------------------------------------------------------------------------------
+// 91. Decode Ways (Medium)
+//---------------------------------------------------------------------------------------
+int numDecodings(string s)
+{
+    // --------------
+    // 6: (6)
+    // 86: (8 6)
+    // --------------
+    // 7: (7)
+    // 27: (2 7)
+    // --------------
+    // 6 : (6)
+    // 26 : (26) (2 6)
+    // 226:  (2 2 6) (22 6) (2 26)
+    // This is similar with: f(n) = f(n - 1) + f(n - 2)
+    // --------------
+    // 826: (8 26) (8 2 6)
+    // --------------
+    // 0 : put 0
+    // 10 : (10)  put 1
+    // 310: (3 10)
+    // --------------
+    // 6: (6)
+    // 06: put 0
+    // 206: (20 6)
+    // --------------
+    // 26: (26) (2 6)
+    // 026 : put 0
+    // 1026: (10 26) (10 2 6)
+    // --------------
+    // 9: (9)
+    // 89: (8 9)
+    // 289: (2 8 9)
+    // --------------
+    // dp[i] = dp[i + 1] + dp[i + 2] if 1 < s[i] <=2
+    //       = dp[i + 1] if s[i] > 2
+    //       = 0 if s[i] = 0
+    //
+
+    const int len = s.size();
+
+    if (len == 0)
+    {
+        return 0;
+    }
+    else if (len == 1)
+    {
+        return s[0] == '0' ? 0 : 1;
+    }
+
+    vector<int> dp(len, 0);
+
+    if (s[len - 1] == '0' && s[len - 2] != '0')
+    {
+        dp[len - 1] = 0;
+        dp[len - 2] = s[len - 2] > '2' ? 0 : 1;
+    }
+    else if(s[len-2] == '0' && s[len - 1] != '0')
+    {
+        dp[len - 1] = 1;
+        dp[len - 2] = 0;
+    }
+    else if(s[len - 1] != '0' && s[len - 2] != '0')
+    {
+        dp[len - 1] = 1;
+        // If the number of two digits is greater than 26, then it is 1, otherwise 2.
+        dp[len - 2] = s[len - 2] > '2' || ( s[len - 2] == '2' && s[len - 1] > '6' ) ? 1 : 2;
+    }
+
+    for (int i = len - 3; i >= 0; --i)
+    {
+        // If number of the last digits is not greater than 26
+        if (s[i] == '0')
+        {
+            dp[i] = 0;
+        }
+        else if (s[i] <= '2' && ( s[i] < '2' || s[i + 1] < '7' )  )
+        {
+            dp[i] = dp[i + 1] + dp[i + 2];
+        }
+        else
+        {
+            dp[i] = dp[i + 1];
+        }
+    }
+
+    return dp[0];
+}
+
+//---------------------------------------------------------------------------------------
+// 139. Word Break (Medium)
+// Topic: Hash table, DP, trie, memorization
+//
+//---------------------------------------------------------------------------------------
+// Solution 1: Recursion with memorization
+class Solution139
+{
+public:
+    bool wordBreak(string s, vector<string>& wordDict)
+    {
+        // Must use memorization, otherwise time limit exceeded.
+        // No reason to use trie, a set is enough.
+        unordered_set<string> wordSet(wordDict.begin(), wordDict.end());
+        // memo[i] means whether the substring s[i~n] can be segmented.
+        // -1: unset. 1: true. 0: false.
+        vector<int> memo(s.size(), -1);
+        return checkSegment(s, wordSet, 0, memo);
+    }
+
+    bool checkSegment(string s, unordered_set<string>& wordSet, int start, vector<int>& memo)
+    {
+        if (start >= s.size())
+        {
+            return true;
+        }
+        if (memo[start] != -1)
+        {
+            return memo[start];
+        }
+
+        // "applepenapple"
+        //       ^...
+        for (int i = start + 1; i <= s.size(); ++i)
+        {
+            if (wordSet.find(s.substr(start, i - start)) != wordSet.end())
+            {
+                bool segmented = checkSegment(s, wordSet, i, memo);
+                if (segmented)
+                {
+                    memo[start] = 1;
+                    return segmented;
+                }
+            }
+        }
+
+        memo[start] = 0;
+        return false;
+    }
+};
+
+// Solution 2: DP
+class Solution139_dp
+{
+public:
+    bool wordBreak(string s, vector<string>& wordDict)
+    {
+        unordered_set<string> wordSet(wordDict.begin(), wordDict.end());
+        //  applepenapple
+        // ^    ^  ^
+        //      j  i
+        // Define dp[i] : whether s.substr(0, i) can be segmented.
+        // dp[i] = dp[j] && substr(j, i - j). j < i.
+        // i starts from 1. dp[0] = true.
+        vector<bool> dp(s.size() + 1, false);
+        dp[0] = true;
+
+        for (int i = 1; i <= s.size(); ++i)
+        {
+            for (int j = 0; j < i; ++j)
+            {
+                if (dp[j] && wordSet.count(s.substr(j, i - j)) > 0)
+                {
+                    dp[i] = true;
+                }
+            }
+        }
+
+        return dp.back();
+    }
+};
+
+//---------------------------------------------------------------------------------------
+// 152. Maximum Product Subarray
+//---------------------------------------------------------------------------------------
+int maxProduct(vector<int>& nums)
+{
+    // This looks like a dynamic programming question but it seems that the space
+    // complexity can be reduced to two variables.
+    if (nums.size() == 0)
+    {
+        return 0;
+    }
+
+    int result = nums[0];
+    // The max product ever seen when reaching i, position i included.
+    int maxProduct = result;
+    // The min product ever seen when reaching i, position i included.
+    int minProduct = result;
+
+    // [2  3  -2  4]
+    //        ^
+    for (int i = 1; i < nums.size(); ++i)
+    {
+        // This is a smart optimization.
+        if (nums[i] < 0)
+        {
+            swap(maxProduct, minProduct);
+        }
+
+        // There is an important catch. When meeting a new number (nums[i]),
+        // the max value could be maxProduct * nums[i] or nums[i].
+        // For example, minProduct = -2, maxProduct = -1, nums[i] = 2.
+        minProduct = min(nums[i], minProduct * nums[i]);
+        maxProduct = max(nums[i], maxProduct * nums[i]);
+
+        result = max(result, maxProduct);
+    }
+
+    return result;
+}
 
 int main()
 {
     cout << "Practice of dynamic programming!\n";
 
     TestPalindromicSubstrings();
+
+    // 91. Decode Ways (Medium)
+    // Input: s = "226"
+    // Output: 3
+    // Explanation : "226" could be decoded as "BZ" ( 2 26 ), "VF" ( 22 6 ), or "BBF" ( 2 2 6 ).
+    // Input: 227 2839 1201234
+    cout << "\n91. Decode Ways: " << numDecodings("1201234") << endl;
+
+    // 139. Word Break (Medium)
+    // Input: s = "applepenapple", wordDict = ["apple","pen"]
+    // Output: true
+    // Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+    // Output: false
+    // "aaaaaaa", { "aaaa","aaa" }
+    // Output: true
+    Solution139_dp sol139;
+    string inputStr = "aaaaaaa";
+    vector<string> inputVS = { "aaaa","aaa" };
+    cout << "\n139. Word Break: " << sol139.wordBreak(inputStr, inputVS);
+    cout << endl;
+
+    // 152. Maximum Product Subarray
+    // Input: nums = [2,3,-2,4]
+    // Output: 6
+    vector<int> inputVI = { 2,3,-2,4 };
+    cout << "\n152. Maximum Product Subarray: " << maxProduct(inputVI) << endl;
 }
